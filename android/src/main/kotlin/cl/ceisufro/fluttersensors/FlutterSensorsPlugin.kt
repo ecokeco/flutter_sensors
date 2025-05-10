96e8b45ec0d50416b4bf33d7d87edd63a83b4dff
Erik Otović <erik.otovic@gmail.com>
2025-05-10 15:20:46 +0200

    Adapted the package to not use deprecated PluginRegistry

--- android/src/main/kotlin/cl/ceisufro/fluttersensors/FlutterSensorsPlugin.kt
+++ android/src/main/kotlin/cl/ceisufro/fluttersensors/FlutterSensorsPlugin.kt
@@ -8,8 +8,13 @@ import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 
 
 class FlutterSensorsPlugin() : FlutterPlugin, MethodCallHandler {
+    companion object {
+        private const val CHANNEL_NAME = "flutter_sensors"
+    }
+
     private var eventChannels = hashMapOf<Int, EventChannel>()
     private var streamHandlers = hashMapOf<Int, SensorStreamHandler>()
+    private lateinit var methodChannel: MethodChannel
     private lateinit var context: Context
     private lateinit var messenger: BinaryMessenger
     private lateinit var sensorManager: SensorManager
@@ -21,37 +26,16 @@ class FlutterSensorsPlugin() : FlutterPlugin, MethodCallHandler {
     }
 
     override fun onAttachedToEngine(binding: FlutterPlugin.FlutterPluginBinding) {
-        val methodChannel = MethodChannel(binding.binaryMessenger, CHANNEL_NAME)
+        methodChannel = MethodChannel(binding.binaryMessenger, CHANNEL_NAME)
+        methodChannel.setMethodCallHandler(this)
         this.context = binding.applicationContext
         this.messenger = binding.binaryMessenger
         this.sensorManager = context.getSystemService(Context.SENSOR_SERVICE) as SensorManager
-        methodChannel.setMethodCallHandler(this)
-    }
-
-    companion object {
-        private const val CHANNEL_NAME = "flutter_sensors"
-
-        @Suppress("deprecation")
-        @JvmStatic
-        fun registerWith(registrar: PluginRegistry.Registrar) {
-            val methodChannel = MethodChannel(registrar.messenger(), CHANNEL_NAME)
-            val context = registrar.context()
-            val binaryMessenger = registrar.messenger()
-            val plugin = FlutterSensorsPlugin(context, binaryMessenger)
-            methodChannel.setMethodCallHandler(plugin)
-            registrar.addViewDestroyListener {
-                plugin.onDestroy()
-                false
-            }
-        }
     }
 
     override fun onDetachedFromEngine(p0: FlutterPlugin.FlutterPluginBinding) {
         removeAllListeners()
-    }
-
-    private fun onDestroy() {
-        removeAllListeners()
+        methodChannel.setMethodCallHandler(null)
     }
 
     private fun removeAllListeners() {
